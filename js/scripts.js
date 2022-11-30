cba543eae4a1aac9daa203a8f0ed4b842a7fa3ed
Lauratell <111022243+Lauratell@users.noreply.github.com>
2022-11-29 22:17:34 -0500

    save

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,29 @@
 console.log("Hi! Welcome to My Portfolio Site")
+
+// Toggle the hamburger menu as visible or not
+
+var viewportWidth = $(window).width();
+
+function menuToggle() {
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle" && viewportWidth < 640) {
+    x.className += " responsive"; //add class name with space "_" to seperate addition
+  } else {
+    x.className = "navtoggle";
+  }
+}
+
+// Hide or show header on scrolling - animation duration set in header CSS
+
+var prev = 0;
+var $window = $(window);
+var nav = $("header");
+
+$window.on("scroll", function () {
+  var scrollTop = $window.scrollTop();
+  if (scrollTop === 0) {
+  } else if (scrollTop > 11) {
+    nav.toggleClass("hidden", scrollTop > prev);
+    prev = scrollTop;
+  }
+});
