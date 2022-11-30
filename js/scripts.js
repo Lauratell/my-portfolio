6dd282de09bcde56d44e242bfd48b3f964bd6687
Lauratell <111022243+Lauratell@users.noreply.github.com>
2022-11-29 22:31:52 -0500

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 console.log("Hi! Welcome to My Portfolio Site")
 
+const hamburgerBtn = document.getElementById("js-hamburger");
+const topLine = document.getElementById("js-top-line");
+const centerLine = document.getElementById("js-center-line");
+const bottomLine = document.getElementById("js-bottom-line");
+const nav = document.getElementById("js-nav");
+
+hamburgerBtn.addEventListener("click", () => {
+  topLine.classList.toggle("active");
+  centerLine.classList.toggle("active");
+  bottomLine.classList.toggle("active");
+  nav.classList.toggle("show");
+});
+
 // Toggle the hamburger menu as visible or not
 
 var viewportWidth = $(window).width();
